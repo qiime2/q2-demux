e00b2160ce958a267f43c804b853c84b8d1874bb
Liz Gehret <elizabeth.gehret@gmail.com>
2022-11-30 13:01:30 -0700

    removing data filepath

--- setup.py
+++ setup.py
@@ -43,7 +43,6 @@ setup(
                            'data/summarize_empty/empty_reverse_in_paired/*',
                            'data/summarize_empty/empty_paired_end/*',
                            'data/reverse_only/*',
-                           'data/*'
                            ],
         'q2_demux': ['_summarize/assets/*.html',
                      '_summarize/assets/dist/*',
