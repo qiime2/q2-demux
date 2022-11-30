f8b6797cbc831f7e32c5607fde8903b61986db78
Liz Gehret <elizabeth.gehret@gmail.com>
2022-11-30 10:58:54 -0700

    fixing data path in setup

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
                            'data/summarize_empty/empty_forward_in_paired/*',
                            'data/summarize_empty/empty_reverse_in_paired/*',
                            'data/summarize_empty/empty_paired_end/*',
-                           'data/reverse_only/*'
+                           'data/reverse_only/*',
                            'data/*'
                            ],
         'q2_demux': ['_summarize/assets/*.html',
