ef2647dd41a50a492f7017bcc16f81fe371e5e81
VinzentRisch <risch.vinzent@gmail.com>
2025-07-15 14:43:08 +0200

    added remove-empty flag to action filter-samples

--- q2_demux/_filter.py
+++ q2_demux/_filter.py
@@ -5,7 +5,7 @@
 #
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
-
+import gzip
 import os
 
 import pandas as pd
@@ -18,22 +18,34 @@ from q2_types.per_sample_sequences import \
 from ._summarize import _PlotQualView
 
 
-def filter_samples(demux: _PlotQualView, metadata: Metadata,
-                   where: str = None, exclude_ids: bool = False) \
+def filter_samples(demux: _PlotQualView, metadata: Metadata = None, where: str = None, 
+                   exclude_ids: bool = False, remove_empty: bool = False) \
                    -> CasavaOneEightSingleLanePerSampleDirFmt:
+    
+    if not any([metadata, remove_empty]):
+        raise ValueError(
+            "At least one of the following parameters must be provided: "
+            "metadata, remove-empty."
+        )
+    
     results = CasavaOneEightSingleLanePerSampleDirFmt()
-
     paired = demux.paired
-    samples = demux.directory_format
-
-    ids_to_keep = metadata.get_ids(where=where)
-    if not ids_to_keep:
-        raise ValueError('No filtering requested.')
+    samples = demux.directory_format    
     manifest = samples.manifest.view(pd.DataFrame)
+    ids_to_keep = set(manifest.index)
+    
+    if metadata is not None:
+        ids_to_keep = metadata.get_ids(where=where)
+        if not ids_to_keep:
+            raise ValueError('No filtering requested.')
 
     if exclude_ids:
         ids_to_keep = set(manifest.index) - set(ids_to_keep)
 
+    if remove_empty:
+        ids_empty = _filter_empty(manifest)
+        ids_to_keep = set(ids_to_keep) - set(ids_empty)
+    
     try:
         for id in ids_to_keep:
             forward = manifest.loc[id].forward
@@ -48,3 +60,27 @@ def filter_samples(demux: _PlotQualView, metadata: Metadata,
                          'demultiplexed data.')
 
     return results
+
+
+def _filter_empty(manifest):
+    """
+    Identify and return sample names from a manifest DataFrame where at least one FASTQ
+    file is empty.
+
+    Parameters:
+        manifest (pandas.DataFrame): A DataFrame where the index contains sample names
+            and the values are file paths pointing to FASTQ files.
+
+    Returns:
+        list: A list of sample names for which at least one associated file is empty
+            (no content on the first line).
+    """
+    empty_samples = []
+    for sample, row in manifest.iterrows():
+        for path in row:
+            if path is not None:
+                with gzip.open(path, "rt") as f:
+                    if not f.readline():
+                        empty_samples.append(sample)
+                        break
+    return empty_samples
--- q2_demux/tests/test_filter.py
+++ q2_demux/tests/test_filter.py
@@ -17,7 +17,7 @@ from q2_types.per_sample_sequences import (
     SingleLanePerSampleSingleEndFastqDirFmt,
     SingleLanePerSamplePairedEndFastqDirFmt)
 
-from q2_demux._filter import filter_samples
+from q2_demux._filter import filter_samples, _filter_empty
 from q2_demux._summarize import _PlotQualView
 
 
@@ -230,6 +230,45 @@ class FilterSamplesTests(TestPluginBase):
                 filter_samples(self.sample_paired, self.md_paired_none, where,
                                True)
 
+    def test_no_parameters_error(self):
+        with self.assertRaisesRegex(ValueError, "At least one of the followin"):
+            filter_samples(self.sample_paired, None, None, False)
+            
+    def test_filter_empty_single(self):
+        obs = _filter_empty(self.manifest_single)
+        exp = ["sample2"]
+        self.assertListEqual(obs, exp)
+    
+    def test_filter_empty_paired(self):
+        obs = _filter_empty(self.manifest_paired)
+        exp = ["sample2"]
+        self.assertListEqual(obs, exp)
+        
+    def test_filter_paired_empty(self):
+        dir_fmt = filter_samples(self.sample_paired, None, None, False, True)
+        self._assert_paired_contains(dir_fmt, ['sample1R1', 'sample1R2'])
+        
+    def test_filter_single_empty(self):
+        dir_fmt = filter_samples(self.sample_single, None, None, False, True)
+        self._assert_paired_contains(dir_fmt, ['sample1R1'])
+
+    def test_filter_single_subset_exclude_empty(self):
+        exps = [(None, []),
+                ("Study='A'", []),
+                ("Study='A' OR Study='B'", [])]
+        for (where, exp) in exps:
+            dir_fmt = filter_samples(self.sample_single,
+                                     self.md_single_subset, where, True, True)
+            self._assert_single_contains(dir_fmt, exp)
+
+    def test_filter_paired_subset_exclude_empty(self):
+        exps = [(None, []),
+                ("Study='A'", []),
+                ("Study='A' OR Study='B'", [])]
+        for (where, exp) in exps:
+            dir_fmt = filter_samples(self.sample_paired,
+                                     self.md_paired_subset, where, True, True)
+            self._assert_paired_contains(dir_fmt, exp)
 
 if __name__ == '__main__':
     unittest.main()
